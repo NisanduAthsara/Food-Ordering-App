c92722eb3f944ebf43846bf3bef006399c114666
NisanduAthsara <nisandunox@gmail.com>
2022-04-11 20:10:52 +0530

    Make cards

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv/config')
 const routes = require('./server/routes/router')
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
 app.use(routes)
+
+app.set('view engine','ejs');
+
+app.use('/css',express.static(path.join(__dirname,'assets/css')))
+app.use('/img',express.static(path.join(__dirname,'assets/img')))
 
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
